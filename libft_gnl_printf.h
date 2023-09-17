5f953a8dd56ea1b7b455b66097dd9f18b2247f3d
remiranda <pesca_botoes.0t@icloud.com>
2023-09-17 05:00:22 +0000

    fix: remove include of gnl and printf headers

--- libft_gnl_printf.h
+++ libft_gnl_printf.h
@@ -6,7 +6,7 @@
 /*   By: rmiranda <rmiranda@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/27 08:17:07 by rmiranda          #+#    #+#             */
-/*   Updated: 2022/08/27 08:17:15 by rmiranda         ###   ########.fr       */
+/*   Updated: 2023/09/15 10:49:03 by rmiranda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 # define LIBFT_GNL_PRINTF_H
 
 # include "./libft/libft.h"
-# include "./printf/ft_printf.h"
-# include "./gnl/get_next_line.h"
+int		ft_printf(const char *format_string, ...);
+char	*ft_get_whole_file(int fd);
+char	*ft_get_next_line(int fd);
 
 #endif
